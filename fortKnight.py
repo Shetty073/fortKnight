ec3e7c7fd26806c33607aa24e64c5832665d157c
Alpha Sierra <30448930+Shetty073@users.noreply.github.com>
2018-04-24 19:36:13 +0530

    new command added
    gets the server status of the game

--- fortKnight.py
+++ fortKnight.py
@@ -5,6 +5,15 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import random
+from bs4 import BeautifulSoup as bs
+import requests as req
+
+#status grabber code starts
+page = req.get("https://lightswitch-public-service-prod06.ol.epicgames.com/lightswitch/api/service/bulk/status?serviceId=Fortnite")
+soup = bs(page.content, "html.parser")
+status = soup.prettify()
+update = "UP"
+#status grabber code ends
 
 bot = commands.Bot(command_prefix='.')
 
@@ -17,5 +26,12 @@ async def ft():
 	places = ["Lucky Landing", "Factories near Dusty Depot", "Motel", "Somewhere between Retail, Jail and Factories", "Football Ground", "Between Shifty and Flush", "Container", "Jail", "North of Wailing Woods", "Anarchy Acres", "Dusty Depot", "Fatal Fields", "Flush Factory", "Greasy Grove", "Haunted Hills", "Junk Junction", "Lonely Lodge", "Loot Lake", "Moisty Mire", "Pleasant Park", "Retail Row", "Salty Springs", "Shifty Shafts", "Snobby Shores", "Tilted Towers", "Tomato Town", "Wailing Woods"]
 	await bot.say(random.choice(places))
 
+#status grabber command code
+@bot.command(pass_context=True)
+async def st():
+	if update in status:
+		await bot.say("Grab your guns! 'cause Fortnite servers are UP and running!")
+	else:
+		await bot.say("Fortnite servers are DOWN!")
 
 bot.run("secret goes here")
